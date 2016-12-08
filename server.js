5c3d4b252c1b03a47f14ae7ed8744a05a5523dbf
Connor Garber <czaneg@gmail.com>
2016-12-07 18:19:17 -0600

    changed to port 5000

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var server = http.createServer(function (req, res) {
   if (m) m.fn(req, res, m.params);
   else ecstatic(req, res)
 });
-server.listen(8000);
+server.listen(5000);
 
 var xcs = new Sender(process.env.SENDER_ID, process.env.SERVER_KEY);
 
